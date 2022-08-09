d0fa93b2d796acce2b33a5281f54d2f25213881b
james etchberger <etchbergerjames@gmail.com>
2022-08-09 16:44:51 -0400

    added more security made sure the fields have to be filled to a certain amount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-
-  const [title, setTitle] = useState('title input')
-  const [description, setDescription] = useState('description text')
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [validForm, setValidForm] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
   const [author, setAuthor] = useState('')
 
-  console.log(title)
+  useEffect(() => {
+    if(title.length > 5 && description.length > 10 && author){
+      setValidForm(true)
+    } else {
+      setValidForm(false)
+    }
+  },[title, description, author])
 
-  function formSubmit(e) {
+  // console.log(title)
+
+  // const forSubmit = async (e) => {
+  async function formSubmit(e) {
     e.preventDefault()
-    console.log('form submitted')
+
+    if (!validForm){
+      setErrorMessage('Not a valid form')
+      return
+    } else {
+      setErrorMessage('')
+    }
+    try {
+
+      // let comment = {
+      //   title: title,
+      //   description: description,
+      //   author: author,
+      // }
+
+      let comment = {
+        title,
+        description,
+        author,
+      }
+      console.log('Form submited with: ', comment)
+
+      //really submit it to an api
+      const results = await fetch('https://sql.bocacode.com/comments', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(comment),
+      })
+      console.log(results)
+      const data = await results.json()
+
+      console.log(data)
+
+      setFormSubmitted(true)
+      alert('Wow! Submitted')
+
+    } catch (error) {
+      console.error(error)
+      setErrorMessage(
+        'There was an error submitting your comment' + error.toString()
+      )
+    }
   }
+
 
 
   return (
@@ -22,40 +77,55 @@ function App() {
 
         {/* Here goes the title */}
         <label>Title</label>
-        <input 
-        type='text' 
-        value={title} 
-        onChange={(e) => {setTitle(e.target.value)}} />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}
+        />
         <h3>{title}</h3>
 
         {/* Here goes the description */}
         <label>Description</label>
-        <textarea 
-        value={description}
-        onChange={(e) => {setDescription(e.target.value)}} />
-        <p><i>{description}</i></p>
-
+        <textarea
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value)
+          }}
+        />
+        <p>
+          <i>{description}</i>
+        </p>
 
         {/* This is the author */}
         <label>Author</label>
-        <select value={author}
-        onChange={(e) => {setAuthor(e.target.value)}}
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value)
+          }}
         >
-          <option value=''>Choose One</option>
-          <option value='todd'>Todd</option>
-          <option value='mason'>Mason</option>
-          <option value='alex'>Alex</option>
-          <option value='james'>James</option>
-          <option value='other'>Other</option>
+          <option value="">Choose One</option>
+          <option value="todd">Todd</option>
+          <option value="mason">Mason</option>
+          <option value="alex">Alex</option>
+          <option value="james">James</option>
+          <option value="">Other</option>
         </select>
         <h3>{author}</h3>
 
-        
-        <button>Submit</button>
-
+        {!formSubmitted && <button>Submit</button>}
+        {errorMessage && (
+          <h1>
+            There was an error:
+            <br />
+            {errorMessage}
+          </h1>
+        )}
       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

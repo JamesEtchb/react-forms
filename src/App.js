8b9bd9bbf9f99c0bc072c82ff773a0d9f1ea6c9e
james etchberger <etchbergerjames@gmail.com>
2022-08-09 14:33:07 -0400

    built out a submit form and styled it a bit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [title, setTitle] = useState('title input')
+  const [description, setDescription] = useState('description text')
+  const [author, setAuthor] = useState('')
+
+  console.log(title)
+
+  function formSubmit(e) {
+    e.preventDefault()
+    console.log('form submitted')
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={formSubmit}>
+        <h1>Comments</h1>
+
+        {/* Here goes the title */}
+        <label>Title</label>
+        <input 
+        type='text' 
+        value={title} 
+        onChange={(e) => {setTitle(e.target.value)}} />
+        <h3>{title}</h3>
+
+        {/* Here goes the description */}
+        <label>Description</label>
+        <textarea 
+        value={description}
+        onChange={(e) => {setDescription(e.target.value)}} />
+        <p><i>{description}</i></p>
+
+
+        {/* This is the author */}
+        <label>Author</label>
+        <select value={author}
+        onChange={(e) => {setAuthor(e.target.value)}}
         >
-          Learn React
-        </a>
-      </header>
+          <option value=''>Choose One</option>
+          <option value='todd'>Todd</option>
+          <option value='mason'>Mason</option>
+          <option value='alex'>Alex</option>
+          <option value='james'>James</option>
+          <option value='other'>Other</option>
+        </select>
+        <h3>{author}</h3>
+
+        
+        <button>Submit</button>
+
+      </form>
     </div>
   );
 }
